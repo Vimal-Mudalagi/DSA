70d9023a3bb6fcd640ab27f6ce4fa66e4c844038
Vimal Mudalagi <134709589+Vimal-Mudalagi@users.noreply.github.com>
2024-01-02 20:42:23 +0530

    PrintFactor

---
+++ Assignments/Conditional and Loops/com/vimal/PrintFactors.java
@@ -0,0 +1,19 @@
+package com.vimal;
+import java.util.Scanner;
+
+public class PrintFactors {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = in.nextInt();
+
+        System.out.println("Factors of number: " + number);
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
